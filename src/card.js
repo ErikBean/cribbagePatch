f8eab123d7595dbdc8f5398a5e6140b581083cb1
Erik Bean <Erik.Bean@cdk.com>
2017-01-21 15:37:03 -0800

    players can select crib cards

--- src/card.js
+++ src/card.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react'
 import { valueOf, getNumberOrFace, getSuit } from './deck'
 
-export default class Card extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      selected: false
-    }
-    this.toggleSelect = this.toggleSelect.bind(this)
+const Card = (props) => {
+  const value = getNumberOrFace(props.card)
+  const suit = getSuit(props.card)
+  const style = {
+    display: 'inline-block',
+    border: props.isSelected ? '3px solid green' : 'none',
+    height: '200px',
+    width: '150px',
+    background: `url(../styles/svg-cards/${value}_of_${suit}.svg) no-repeat`,
+    backgroundSize: 'contain'
   }
-  toggleSelect() {
-    this.setState({
-      selected: !this.state.selected
-    })
-  }
-  render() {
-    const value = getNumberOrFace(this.props.card)
-    const suit = getSuit(this.props.card)
-    const style = {
-      display: 'inline-block',
-      border: this.state.selected ? '3px solid green' : 'none',
-      height: '200px',
-      width: '150px',
-      background: `url(../styles/svg-cards/${value}_of_${suit}.svg) no-repeat`,
-      backgroundSize: 'contain'
-    }
-    return (
-      <div onClick={this.toggleSelect} style={style}/>
-    )
-  }
+  return (
+    <div onClick={props.toggleSelect} style={style}/>
+  )
 }
+export default Card
--- src/hand.js
+++ src/hand.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { without, includes } from 'lodash'
 import Card from './card'
 
-const Hand = (props) => (
-  <div>
-    {props.hand.map((card) =>{
-      return <Card card={card} key={card}/>
-    })}
-  </div>
-)
-export default Hand
+export default class Hand extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      cards: props.cards,
+      selected: [null, null]
+    }
+    this.toggleSelect = this.toggleSelect.bind(this)
+  }
+  toggleSelect(card) {
+    if(includes(this.state.selected, card)){
+      this.setState({
+        selected: [null].concat(without(this.state.selected, card))
+      })
+    } else {
+      this.setState({
+        selected: [card, this.state.selected[0]]
+      })
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.props.hand.map((card) => (
+          <Card 
+            toggleSelect={this.toggleSelect.bind(null,card)}
+            isSelected={includes(this.state.selected, card)}
+            card={card}
+            key={card} />
+        ))}
+      </div>
+    )
+  }
+}
