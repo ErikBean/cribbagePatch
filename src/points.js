c699cc49ad3fbf4c0c356400fd04606acf83ece0
Erik Bean <Erik.Bean@cdk.com>
2017-01-22 14:26:00 -0800

    extend to matrices of arbitrary dimensions

--- src/points.js
+++ src/points.js
@@ -18,22 +18,33 @@ export function isFifteen(...cards) {
 
 window.doubles = []
 window.triples = []
+
 export function getFifteens (hand) {
   let x2 = []
   let x3 = []
   window.doubles.push(x2)
   window.triples.push(x3)
-  const isSameCard = (cardA, cardB) => (hand.indexOf(cardA) === hand.indexOf(cardB)) 
   // bottom diagonal of matrix, same pairs in reversed order:
-  const isOutsideSparseMatrix = (cardA, cardB) => (hand.indexOf(cardA) > hand.indexOf(cardB))
+  const isOutsideSparseMatrix = (cardA, cardB) => ( hand.indexOf(cardA) >= hand.indexOf(cardB) )
+  const isOutsideMatrix = (...cards) => {
+    // indices must be in order! A > B > C > D 
+    const indices = cards.map((c) => hand.indexOf(c))
+    for(let i=1; i < indices.length; i++){
+      const thisIndex = indices[i]
+      const prevIndex = indices[i - 1]
+      if(thisIndex >= prevIndex){
+        return true
+      }
+    }
+    return false
+  }
+  
   for(let cardA of hand){
     for(let cardB of hand){
-      if(isSameCard(cardA, cardB)) continue 
-      if(isOutsideSparseMatrix(cardA, cardB)) continue
+      if(isOutsideMatrix(cardA, cardB)) continue
       if(isFifteen(cardA, cardB)) x2.push([cardA, cardB])
       for(let cardC of hand){
-        if(isSameCard(cardA, cardC) || isSameCard(cardB, cardC)) continue
-        if(isOutsideSparseMatrix(cardA, cardC) || isOutsideSparseMatrix(cardB, cardC)) continue
+        if(isOutsideMatrix(cardA, cardB, cardC)) continue
         if(isFifteen(cardA, cardB, cardC)) x3.push([cardA, cardB, cardC])
       }
     }
