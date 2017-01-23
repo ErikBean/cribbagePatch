a56a55c277e5c1d06cf665e2de310f3bcb955547
Erik Bean <Erik.Bean@cdk.com>
2017-01-22 17:22:59 -0800

    move hand class methods to state

--- src/hand.js
+++ src/hand.js
@@ -5,21 +5,33 @@ import Card from './card'
 import Line from './line'
 
 export default class Hand extends Component {
-  constructor(props){
+  constructor (props) {
     super(props)
+    const fifteens = getFifteens(props.hand)
+    const pairs = getPairs(props.hand)
+    console.log('>>> stuff! ! : ', {fifteens, pairs})
     this.state = {
       selected: [null, null], // Should always be length 2
-      fifteens: getFifteens(this.props.hand),
-      pairs: getPairs(this.props.hand), // 3 of a kind if any prop has value > 1
-      runs: getRuns(this.props.hand)
+      fifteens, 
+      pairs,
+      runs: getRuns(props.hand).length,
+      numFifteens: Object.keys(fifteens).map((k) => {
+        return fifteens[k].length
+      }).reduce((acc, curr) => {
+        return acc + curr
+      }, 0),
+      numPairs: Object.keys(pairs).map((k) => {
+        return pairs[k]
+      }).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
     }
     this.toggleSelect = this.toggleSelect.bind(this)
-    this.numFifteens = this.numFifteens.bind(this)
   }
-  toggleSelect(card) {
-    if(includes(this.state.selected, card)){
+  toggleSelect (card) {
+    if (includes(this.state.selected, card)) {
       this.setState({
-        selected: [null].concat(without(this.state.selected, card))
+        selected: [null, ...without(this.state.selected, card)]
       })
     } else {
       this.setState({
@@ -27,39 +39,25 @@ export default class Hand extends Component {
       })
     }
   }
-  numFifteens(){
-    return Object.keys(this.state.fifteens).map((k) =>{
-      return this.state.fifteens[k].length
-    }).reduce((acc, curr) => {
-      return acc + curr
-    }, 0)
-  }
-  numPairs(){
-    return Object.keys(this.state.pairs).map((k) => {
-      return this.state.pairs[k]
-    }).reduce((acc, curr) => {
-      return acc + curr
-    }, 0)
-  }
   render () {
     const renderCard = (card) => (
       <Card
-        toggleSelect={this.toggleSelect.bind(null,card)}
+        toggleSelect={this.toggleSelect.bind(null, card)}
         isSelected={includes(this.state.selected, card)}
         card={card}
         key={card} />
-    )    
+    )
     return (
       <div>
         <ul>
           <li>
-            Fifteens: {this.numFifteens()}
+            Fifteens: {this.state.numFifteens}
           </li>
           <li>
-            pairs:{this.numPairs()}
+            pairs: {this.state.numPairs}
           </li>
           <li>
-            runs:{JSON.stringify(this.state.runs)}
+            runs: {this.state.runs}
           </li>
         </ul>
         {this.props.hand.map(renderCard)}
