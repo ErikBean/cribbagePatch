8716c7951c73d37e9bee5a6a4bad368e69c6b77f
Erik Bean <Erik.Bean@cdk.com>
2017-01-29 11:37:55 -0800

    three and four-of-a-kind

--- src/points.js
+++ src/points.js
@@ -1,8 +1,8 @@
 import { valueOf, getSuit } from './deck'
-import { uniq, size, toPairs } from 'lodash'
+import { uniq, size, toPairs, times } from 'lodash'
 
-export default function getPoints(hand){
-  //game logic ....
+export default function getPoints (hand) {
+  // game logic ....
   const values = hand.map(valueOf)
   const suits = hand.map(getSuit)
   const fifteens = getFifteens(values)
@@ -12,16 +12,16 @@ function valueMaxTen (card) {
   return valueOf(card) > 10 ? 10 : valueOf(card)
 }
 
-export function isFifteen(...cards) {
-  let sum = cards.map((card) => valueMaxTen(card)).reduce((acc, curr) => (acc+curr), 0)
+export function isFifteen (...cards) {
+  let sum = cards.map((card) => valueMaxTen(card)).reduce((acc, curr) => (acc + curr), 0)
   return sum === 15
 }
 
 function isArraySorted (array) {
-  for(let i=1; i < array.length; i++){
+  for (let i = 1; i < array.length; i++) {
     const thisIndex = array[i]
     const prevIndex = array[i - 1]
-    if(thisIndex >= prevIndex){
+    if (thisIndex >= prevIndex) {
       return true
     }
   }
@@ -29,39 +29,47 @@ function isArraySorted (array) {
 }
 
 export function getFifteens (hand) {
-  const fifteens = {1:[], 2:[], 3:[], 4:[], 5:[]} // index = number of cards in combos
-  
+  const fifteens = {1: [], 2: [], 3: [], 4: [], 5: []} // index = number of cards in combos
+
   const isOutsideMatrix = (...cards) => {
     // Skip over elements outside traingular matrix (dimensions of matrix = cards.length)
-    // Indices must be in order (A > B > C > D) to be strictly trianglular 
+    // Indices must be in order (A > B > C > D) to be strictly trianglular
     const indices = cards.map((c) => hand.indexOf(c))
     return isArraySorted(indices)
   }
   // Compare hand to itelf on 5 axes, to determine all 5-card combos (five-dimensional matrix)
   // Since matrix is symetric and addition is commutitive, only compute trianglular matrix
-  function calcFifteens(otherCards){
-    if(otherCards.length >= 5) return
-    for(let cardX of hand){
-      if(isOutsideMatrix(cardX, ...otherCards)) continue // keep matrix sparse
-      if(isFifteen(cardX, ...otherCards)) fifteens[otherCards.length + 1].push([cardX, ...otherCards])
+  function calcFifteens (otherCards) {
+    if (otherCards.length >= 5) return
+    for (let cardX of hand) {
+      if (isOutsideMatrix(cardX, ...otherCards)) continue // keep matrix sparse
+      if (isFifteen(cardX, ...otherCards)) fifteens[otherCards.length + 1].push([cardX, ...otherCards])
       calcFifteens([cardX, ...otherCards])
     }
   }
-  
-  for(let card of hand){
+
+  for (let card of hand) {
     calcFifteens([card])
   }
   return fifteens
 }
 
-// TODO: Does not work for 3 of a kind: last and first don't get matched
 export function getPairs (hand) {
   const values = hand.map(valueOf).sort()
   const pairs = {}
-  //if number occurs twice in a row, add to pairs object
-  for (let i = 1;i < values.length; i++){
-    if(values[i] === values[i - 1]){
+  // if number occurs twice in a row, add to pairs object
+  for (let i = 1; i < values.length; i++) {
+    if (values[i] === values[i - 1]) {
       pairs[values[i]] ? pairs[values[i]]++ : pairs[values[i]] = 1
+    }
+  }
+  for (let n in pairs) {
+    const hasThreeOfAKind = pairs[n] === 2
+    const hasFourOfAKind = pairs[n] === 3
+    if (hasThreeOfAKind) {
+      pairs[n] = 3
+    } else if (hasFourOfAKind) {
+      pairs[n] = 6 
     }
   }
   return pairs
@@ -70,20 +78,22 @@ export function getPairs (hand) {
 export function getRuns (hand) {
   const values = uniq(hand.map(valueOf).sort((a, b) => a > b))
   const run = []
-  for (let i = 1;i < values.length; i++){
+  for (let i = 1; i < values.length; i++) {
     const thisVal = values[i]
     const lastVal = values[i - 1]
-    if(thisVal === (lastVal + 1)){
+    if (thisVal === (lastVal + 1)) {
       run.length ? run.push(thisVal) : run.push(lastVal, thisVal)
     }
   }
-  if(run.length < 3) return [] // no runs
-  const runs = [ run ]
+  if (run.length < 3) return [] // no runs
   const pairs = toPairs(getPairs(hand))
-  if(!pairs.length) return runs // no double run, because no pairs
-  for(let [value, numPairs] of pairs ){
-    if(run.includes(value)){ // has double run
-      
+  if (!pairs.length) return [ run ] // no double run, because no pairs
+  // compute doubles:
+  let runs = []
+  for (let [value, numPairs] of pairs) {
+    if (run.includes(parseInt(value))) { // has double run
+      const duplicates = numPairs + 1
+      runs = runs.concat(times(duplicates, () => run))
     }
   }
   return runs
